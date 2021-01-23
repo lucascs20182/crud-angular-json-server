1e67e125b1c6107ca780324d13ebf563c667c151
lucascs20182 <lucascs20182@gmail.com>
2021-01-23 11:14:08 -0300

    implementado alter do nome do componente no header

---
+++ frontend/src/app/components/template/header/header.service.ts
@@ -0,0 +1,24 @@
+import { HeaderData } from './header-data.model';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+  private _headerData = new BehaviorSubject<HeaderData>({
+    title: 'Início',
+    icon: 'home',
+    routeUrl: ''
+  })
+
+  constructor() { }
+
+  get HeaderData() : HeaderData {
+    return this._headerData.value
+  }
+
+  set HeaderData(headerData: HeaderData) {
+    this._headerData.next(headerData)
+  }
+}
