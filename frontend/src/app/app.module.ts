c340b7d19667cc9121e1aa759095fb7ee2fce1ff
lucascs20182 <lucascs20182@gmail.com>
2021-01-23 10:20:49 -0300

    implementado edição de produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -48,6 +49,7 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductUpdateComponent,
     // ProductReadTempComponent
   ],
   imports: [
